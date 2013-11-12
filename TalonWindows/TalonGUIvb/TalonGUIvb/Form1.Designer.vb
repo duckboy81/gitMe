e13195a0023a26de5a8abf8b830c128fffb424ed
Jean-Luc Duckworth <c14jeanluc.duckworth@usafa.edu>
2013-11-11 20:14:30 -0700

    Beautiful VB GUI // Program is able to RX messages from MMTTY

---
+++ TalonWindows/TalonGUIvb/TalonGUIvb/Form1.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.BatteryBar = New System.Windows.Forms.ProgressBar()
+        Me.ResetButton = New System.Windows.Forms.Button()
+        Me.IgnoreDataButton = New System.Windows.Forms.Button()
+        Me.detectionsLabel = New System.Windows.Forms.Label()
+        Me.longitudeLabel = New System.Windows.Forms.Label()
+        Me.batteryLabel = New System.Windows.Forms.Label()
+        Me.latitudeLabel = New System.Windows.Forms.Label()
+        Me.statusLabel = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RockListAnimationTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.MMTTY_Text = New System.Windows.Forms.RichTextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.BatteryBar)
+        Me.GroupBox1.Controls.Add(Me.ResetButton)
+        Me.GroupBox1.Controls.Add(Me.IgnoreDataButton)
+        Me.GroupBox1.Controls.Add(Me.detectionsLabel)
+        Me.GroupBox1.Controls.Add(Me.longitudeLabel)
+        Me.GroupBox1.Controls.Add(Me.batteryLabel)
+        Me.GroupBox1.Controls.Add(Me.latitudeLabel)
+        Me.GroupBox1.Controls.Add(Me.statusLabel)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!)
+        Me.GroupBox1.Location = New System.Drawing.Point(14, 8)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(600, 140)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Rock Status"
+        '
+        'BatteryBar
+        '
+        Me.BatteryBar.Location = New System.Drawing.Point(262, 105)
+        Me.BatteryBar.Name = "BatteryBar"
+        Me.BatteryBar.Size = New System.Drawing.Size(127, 23)
+        Me.BatteryBar.TabIndex = 4
+        '
+        'ResetButton
+        '
+        Me.ResetButton.Enabled = False
+        Me.ResetButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ResetButton.Location = New System.Drawing.Point(533, 104)
+        Me.ResetButton.Name = "ResetButton"
+        Me.ResetButton.Size = New System.Drawing.Size(56, 25)
+        Me.ResetButton.TabIndex = 3
+        Me.ResetButton.Text = "Reset"
+        Me.ResetButton.UseVisualStyleBackColor = True
+        '
+        'IgnoreDataButton
+        '
+        Me.IgnoreDataButton.Enabled = False
+        Me.IgnoreDataButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.IgnoreDataButton.Location = New System.Drawing.Point(430, 104)
+        Me.IgnoreDataButton.Name = "IgnoreDataButton"
+        Me.IgnoreDataButton.Size = New System.Drawing.Size(94, 25)
+        Me.IgnoreDataButton.TabIndex = 3
+        Me.IgnoreDataButton.Text = "Ignore Data"
+        Me.IgnoreDataButton.UseVisualStyleBackColor = True
+        '
+        'detectionsLabel
+        '
+        Me.detectionsLabel.AutoSize = True
+        Me.detectionsLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.detectionsLabel.Location = New System.Drawing.Point(537, 83)
+        Me.detectionsLabel.Name = "detectionsLabel"
+        Me.detectionsLabel.Size = New System.Drawing.Size(0, 16)
+        Me.detectionsLabel.TabIndex = 2
+        '
+        'longitudeLabel
+        '
+        Me.longitudeLabel.AutoSize = True
+        Me.longitudeLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.longitudeLabel.Location = New System.Drawing.Point(508, 54)
+        Me.longitudeLabel.Name = "longitudeLabel"
+        Me.longitudeLabel.Size = New System.Drawing.Size(0, 16)
+        Me.longitudeLabel.TabIndex = 2
+        '
+        'batteryLabel
+        '
+        Me.batteryLabel.AutoSize = True
+        Me.batteryLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.batteryLabel.Location = New System.Drawing.Point(322, 83)
+        Me.batteryLabel.Name = "batteryLabel"
+        Me.batteryLabel.Size = New System.Drawing.Size(0, 16)
+        Me.batteryLabel.TabIndex = 2
+        '
+        'latitudeLabel
+        '
+        Me.latitudeLabel.AutoSize = True
+        Me.latitudeLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!)
+        Me.latitudeLabel.Location = New System.Drawing.Point(329, 54)
+        Me.latitudeLabel.Name = "latitudeLabel"
+        Me.latitudeLabel.Size = New System.Drawing.Size(0, 16)
+        Me.latitudeLabel.TabIndex = 2
+        '
+        'statusLabel
+        '
+        Me.statusLabel.AutoSize = True
+        Me.statusLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.statusLabel.Location = New System.Drawing.Point(259, 24)
+        Me.statusLabel.MaximumSize = New System.Drawing.Size(330, 0)
+        Me.statusLabel.MinimumSize = New System.Drawing.Size(330, 0)
+        Me.statusLabel.Name = "statusLabel"
+        Me.statusLabel.Size = New System.Drawing.Size(330, 20)
+        Me.statusLabel.TabIndex = 2
+        Me.statusLabel.Text = "No Rock Selected"
+        Me.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label7.Location = New System.Drawing.Point(426, 80)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(103, 20)
+        Me.Label7.TabIndex = 1
+        Me.Label7.Text = "# Detections:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label6.Location = New System.Drawing.Point(426, 51)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(84, 20)
+        Me.Label6.TabIndex = 1
+        Me.Label6.Text = "Longitude:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label2.Location = New System.Drawing.Point(258, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Battery:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.Label3.Location = New System.Drawing.Point(258, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 20)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Latitude:"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Location = New System.Drawing.Point(11, 24)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(233, 104)
+        Me.ListBox1.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.MMTTY_Text)
+        Me.GroupBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox2.Location = New System.Drawing.Point(14, 153)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(600, 300)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Rock Mapping"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(487, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(121, 18)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Populate Rock List"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RockListAnimationTimer
+        '
+        Me.RockListAnimationTimer.Enabled = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 6.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(305, 0)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(114, 18)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Start MMTTY"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'MMTTY_Text
+        '
+        Me.MMTTY_Text.Location = New System.Drawing.Point(12, 77)
+        Me.MMTTY_Text.Name = "MMTTY_Text"
+        Me.MMTTY_Text.Size = New System.Drawing.Size(576, 157)
+        Me.MMTTY_Text.TabIndex = 1
+        Me.MMTTY_Text.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(624, 462)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(640, 500)
+        Me.MinimumSize = New System.Drawing.Size(640, 500)
+        Me.Name = "Form1"
+        Me.Text = "Talon Rock Monitor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents statusLabel As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents longitudeLabel As System.Windows.Forms.Label
+    Friend WithEvents latitudeLabel As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents detectionsLabel As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents ResetButton As System.Windows.Forms.Button
+    Friend WithEvents BatteryBar As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents batteryLabel As System.Windows.Forms.Label
+    Friend WithEvents IgnoreDataButton As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents RockListAnimationTimer As System.Windows.Forms.Timer
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents MMTTY_Text As System.Windows.Forms.RichTextBox
+
+End Class
---
+++ TalonWindows/TalonGUIvb/TalonGUIvb/Form1.vb
@@ -0,0 +1,281 @@
+﻿Option Explicit On
+Public Class Form1
+    Private Declare Function FindWindow Lib "user32.dll" Alias "FindWindowA" (ByVal lpClassName As String, ByVal lpWindowName As String) As IntPtr
+    Private Declare Function FindWindowEx Lib "user32.dll" Alias "FindWindowExA" (ByVal hWndParent As IntPtr, ByVal hWndChildAfter As Integer, ByVal lpClassName As String, ByVal lpWindowName As String) As IntPtr
+    Private Declare Function SendMessage Lib "user32.dll" Alias "SendMessageA" (ByVal hWnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As IntPtr) As IntPtr
+    Private Declare Function PostMessage Lib "user32.dll" Alias "PostMessageA" (ByVal hwnd As IntPtr, ByVal wMsg As Integer, ByVal wParam As Integer, ByVal lParam As IntPtr) As IntPtr
+    'Private Declare Function RegisterWindowMessage Lib "user32" Alias "RegisterWindowMessageA" (ByVal lpString As String) As Long
+    Private Declare Function RegisterWindowMessage Lib "user32.dll" Alias "RegisterWindowMessageA" (ByVal lpString As String) As UInteger
+
+    Private Class rock
+        Public rockName As String
+        Public numDetections As Integer
+        Public posLatitude As Double
+        Public posLongitude As Double
+        Public dataIgnored As Boolean
+        Public status As String
+        Public batteryLevel As Integer
+        Public updatedAnimationCount As Integer
+        Public updatedString As String
+    End Class
+
+    'MMTTY -> APP 
+    Enum FROM_MMTTY
+        TXM_HANDLE = &H8000
+        TXM_REQHANDLE
+        TXM_START
+        TXM_CHAR
+        TXM_PTTEVENT
+    End Enum
+
+    'APP -> MMTTY
+    Enum TO_MMTTY
+        RXM_HANDLE = &H0
+        RXM_REQHANDLE
+        RXM_EXIT
+        RXM_PTT
+        RXM_CHAR
+
+        RXM_WINPOS
+        RXM_WIDTH
+        RXM_REQPARA
+        RXM_SETBAUD
+        RXM_SETMARK
+
+        RXM_SETSPACE
+        RXM_SETSWITCH
+        RXM_SETHAM
+        RXM_SHOWSETUP
+        RXM_SETVIEW
+
+        RXM_SETSQLVL
+        RXM_SHOW
+        RXM_SETFIG
+        RXM_SETRESO
+        RXM_SETLPF
+
+        RXM_SETTXDELAY
+        RXM_UPDATECOM
+        RXM_SUSPEND
+        RXM_NOTCH
+        RXM_PROFILE
+
+        RXM_TIMER
+        RXM_ENBFOCUS
+        RXM_SETDEFFREQ
+        RXM_SETLENGTH
+    End Enum
+
+    Dim MSG_MMTTY As Long = RegisterWindowMessage("MMTTY")
+    Dim MMTTY_Handle As Long = vbNull
+    Dim MMTTY_ThreadId As Long = vbNull
+    Dim MMTTY_process As System.Diagnostics.Process
+
+    Dim rockList As ArrayList = New ArrayList
+    Dim rockCount As Integer = 1
+    Dim numChar2ShowUpdate As Integer = 8
+
+    Private Sub PaintMap(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles GroupBox2.Paint
+        'Handles Me.Paint
+
+        Dim blueBrush As New Drawing.SolidBrush(Color.Blue)
+        Dim whiteBrush As New Drawing.SolidBrush(Color.White)
+        Dim redBrush As New Drawing.SolidBrush(Color.Red)
+        Dim blackBrush As New Drawing.SolidBrush(Color.Black)
+        Dim greenBrush As New Drawing.SolidBrush(Color.Green)
+
+        e.Graphics.FillRectangle(blackBrush, 20 - 1, 30 - 1, GroupBox2.Width - 40 + 2, GroupBox2.Height - 50 + 2)
+        e.Graphics.FillRectangle(blueBrush, 20, 30, GroupBox2.Width - 40, GroupBox2.Height - 50)
+
+    End Sub
+
+
+    Private Sub AddRockToList(thisRock As rock)
+        ListBox1.Items.Add(thisRock.rockName)
+    End Sub
+
+    Private Sub ListBox1_SelectedValueChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedValueChanged, ListBox1.SelectedIndexChanged
+
+        For Each thisRock As rock In rockList
+            IgnoreDataButton.Enabled = False
+            ResetButton.Enabled = False
+
+            If (ListBox1.SelectedItem.ToString.Contains(thisRock.rockName)) Then
+                statusLabel.Text = thisRock.status
+                detectionsLabel.Text = thisRock.numDetections
+                latitudeLabel.Text = thisRock.posLatitude
+                longitudeLabel.Text = thisRock.posLongitude
+                batteryLabel.Text = thisRock.batteryLevel.ToString + "%"
+                BatteryBar.Value = thisRock.batteryLevel
+
+                IgnoreDataButton.Text = "Ignore Data"
+                If (thisRock.dataIgnored = True) Then
+                    IgnoreDataButton.Text = "Allow Data"
+                End If
+
+                IgnoreDataButton.Enabled = True
+                ResetButton.Enabled = True
+                Exit For
+            End If
+        Next
+
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim tempRock As rock
+
+        tempRock = New rock
+        tempRock.rockName = "Rock " + rockCount.ToString
+        rockCount = rockCount + 1
+        tempRock.numDetections = 4
+        tempRock.posLatitude = 32.40239
+        tempRock.posLongitude = -103.123112
+        tempRock.dataIgnored = False
+        tempRock.status = "Listening"
+        tempRock.batteryLevel = 46
+        tempRock.updatedString = "[-UPDATED--UPDATED-]"
+        tempRock.updatedAnimationCount = tempRock.updatedString.Length + 6
+        rockList.Add(tempRock)
+        AddRockToList(tempRock)
+
+        tempRock = New rock
+        tempRock.rockName = "Rock " + rockCount.ToString
+        rockCount = rockCount + 1
+        tempRock.numDetections = 14
+        tempRock.posLatitude = 33.40239
+        tempRock.posLongitude = -101.123112
+        tempRock.dataIgnored = False
+        tempRock.status = "Obtaining GPS Data"
+        tempRock.batteryLevel = 83
+        rockList.Add(tempRock)
+        AddRockToList(tempRock)
+
+        tempRock = New rock
+        tempRock.rockName = "Rock " + rockCount.ToString
+        rockCount = rockCount + 1
+        tempRock.numDetections = 0
+        tempRock.posLatitude = 31.40239
+        tempRock.posLongitude = -102.123112
+        tempRock.dataIgnored = False
+        tempRock.status = "Listening"
+        tempRock.batteryLevel = 49
+        rockList.Add(tempRock)
+        AddRockToList(tempRock)
+    End Sub
+
+    Private Sub IgnoreDataButton_Click(sender As Object, e As EventArgs) Handles IgnoreDataButton.Click
+        For Each thisRock As rock In rockList
+            If (ListBox1.SelectedItem = thisRock.rockName) Then
+                thisRock.dataIgnored = Not (thisRock.dataIgnored)
+
+                Exit For
+            End If
+        Next
+
+        'Update the current information
+        Call ListBox1_SelectedValueChanged(Nothing, Nothing)
+    End Sub
+
+    Private Sub ResetButton_Click(sender As Object, e As EventArgs) Handles ResetButton.Click
+        For Each thisRock As rock In rockList
+            If (ListBox1.SelectedItem = thisRock.rockName) Then
+                thisRock.batteryLevel = 0
+                thisRock.dataIgnored = False
+                thisRock.numDetections = 0
+                thisRock.posLatitude = 0
+                thisRock.posLongitude = 0
+                thisRock.status = "Awaiting Next Data Pulse"
+                Exit For
+            End If
+        Next
+
+        'Update the current information
+        Call ListBox1_SelectedValueChanged(Nothing, Nothing)
+    End Sub
+
+    Private Sub RockListAnimationTimer_Tick(sender As Object, e As EventArgs) Handles RockListAnimationTimer.Tick
+        Dim startSubString As Integer
+        Dim currStringLength As Integer
+        Dim currUpdateString As String = ""
+        Dim currIndex As Integer = ListBox1.TopIndex()
+
+        For Each thisRock As rock In rockList
+            If (thisRock.updatedAnimationCount > 0) Then
+
+                'Change the below spaces to increase or decrease amount shown
+                currUpdateString = MakeSpaces(numChar2ShowUpdate) + thisRock.updatedString + MakeSpaces(numChar2ShowUpdate)
+                currStringLength = currUpdateString.Length
+
+                startSubString = currStringLength - thisRock.updatedAnimationCount - numChar2ShowUpdate + 1
+
+                If (startSubString >= 0) Then
+
+                    currUpdateString = currUpdateString.Substring(startSubString, numChar2ShowUpdate)
+
+                    For i As Integer = 0 To ListBox1.Items.Count
+                        'For Each rockName As String In ListBox1.Items
+                        If (ListBox1.Items(i).ToString.Contains(thisRock.rockName)) Then
+                            ListBox1.Items.Item(i) = thisRock.rockName + "    " + currUpdateString
+                            Exit For
+                        End If
+                    Next
+
+                    thisRock.updatedAnimationCount = thisRock.updatedAnimationCount - 1
+                End If
+            End If
+        Next
+
+        ListBox1.TopIndex = currIndex
+    End Sub
+
+    Private Function MakeSpaces(numberSpaces As Integer) As String
+        Dim newString As String = ""
+
+        For i As Integer = 1 To numberSpaces
+            newString = newString + " "
+        Next
+
+        Return newString
+    End Function
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        Dim hWnd As IntPtr = m.HWnd
+        Dim message As Integer = m.Msg
+        Dim wParam As IntPtr = m.WParam
+        Dim lParam As IntPtr = m.LParam
+
+        'Show the Message "OK" if someone sends a message with MSG_MMTTY:
+        Select Case message
+            Case WindowMessages.WM_DESTROY
+                PostMessage(MMTTY_Handle, MSG_MMTTY, TO_MMTTY.RXM_EXIT, vbNull)
+            Case Else
+                If (message = MSG_MMTTY) Then
+                    Select Case wParam
+                        Case FROM_MMTTY.TXM_HANDLE
+                            MMTTY_Handle = lParam
+                            PostMessage(MMTTY_Handle, MSG_MMTTY, TO_MMTTY.RXM_HANDLE, hWnd)
+                        Case FROM_MMTTY.TXM_CHAR
+                            MMTTY_Text.Text = MMTTY_Text.Text + Chr(lParam.ToString)
+                    End Select
+                End If
+        End Select
+
+        MyBase.WndProc(m)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        MMTTY_process = Process.Start("C:\Users\C14JeanLuc.Duckworth\Downloads\MMTTY.EXE", "-r")
+
+        'PostMessage(MMTTY_Handle, MSG_MMTTY, TO_MMTTY.RXM_PTT, 2)
+
+        Dim hwnd As IntPtr = FindWindow(vbNullString, "Virtual CDRom Control Panel")
+        Dim x As IntPtr = FindWindowEx(hwnd, 0, vbNullString, "Driver Control ...")
+
+        'PostMessage(x, BM_CLICK, 0&, 0&)
+        'Thread.Sleep(200)
+        hwnd = FindWindow(vbNullString, "Virtual CD-ROM Driver Control")
+        Debug.Print(hwnd)
+    End Sub
+End Class
+
